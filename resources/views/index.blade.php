755993adf5fb824297fd4af5602477d78a1dd4af
Gustavo <gustavo18op@hotmail.com>
2022-02-24 17:28:35 -0600

    Blade templaries

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.layout')
+@section('title', 'Representante')
+@section('content')
+    <h1>Representante</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Ciudad</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Apellido</th>
+                <th scope="col">Telefono</th>
+                <th scope="col">Correo</th>
+                <th scope="col">Pais</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+            </tr>
+        </tbody>
+    </table>
+@endsection
